e403f901e11c57dec351be91d9fb3f0b95bf61d4
meerestier <lars.schulz@systemic.de>
2016-09-24 08:24:04 +0200

    Running, but with caveats

--- ledaps_localized/Process.sh
+++ ledaps_localized/Process.sh
@@ -16,8 +16,20 @@ docker build -t natriskchange/ledaps .
 # -v /Volumes/DATA_DRIVE/NatRiskChange/Results:/results \
 # natriskchange/ledaps /bin/bash
 
-docker run -ti --rm --name=ledaps_image \
+
+# Name the Scene in the data folder
+echo "Please paste the Landsat Scene ID to process. (example: LT41760361990224XXX03)"
+read sceneID
+echo $sceneID
+
+docker run -ti --rm \
 -v /Volumes/DATA_DRIVE/NatRiskChange/Auxiliary/ledaps_aux.1978-2014:/opt/ledaps \
--v /Volumes/DATA_DRIVE/NatRiskChange/Data/LT51760361987032ESA00:/data \
+-v /Volumes/DATA_DRIVE/NatRiskChange/Data:/data \
 -v /Volumes/DATA_DRIVE/NatRiskChange/Results:/results \
-natriskchange/ledaps /data/LT51760361987032ESA00/ /opt/ledaps/
+natriskchange/ledaps /data/$sceneID /opt/ledaps
+
+# docker run -ti --name=ledaps_image \
+# -v /Volumes/DATA_DRIVE/NatRiskChange/Auxiliary/ledaps_aux.1978-2014:/opt/ledaps \
+# -v /Volumes/DATA_DRIVE/NatRiskChange/Data:/data \
+# -v /Volumes/DATA_DRIVE/NatRiskChange/Results:/results \
+# natriskchange/ledaps /data/$sceneID /opt/ledaps
