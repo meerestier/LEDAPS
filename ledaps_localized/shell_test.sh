efe6a044443fa3be2ce731bc6e520e27ee92edb0
meerestier <lars.schulz@systemic.de>
2016-09-26 15:14:44 +0200

    Prepare iteration

--- ledaps_localized/shell_test.sh
+++ ledaps_localized/shell_test.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-echo -n "Enter Source Directory:"
-read srcdir
-echo $srcdir
 
-read -p "Are you sure? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-echo "good"
-fi
+
+# Name the Scene in the data folder
+echo "Please paste the Landsat Scene ID to process. (example: LT41760361990224XXX03)"
+read sceneID
+
+## declare an array variable
+declare -a arr=($sceneID)
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+  # or do whatever with individual element of the array
+  echo  "Processing $i"
+done
+
+# You can access them using echo "${arr[0]}", "${arr[1]}" also
